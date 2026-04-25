18e896a3ae3743513223d940b65523349393434e
daoviethung0911 <140297578+daoviethung0911@users.noreply.github.com>
2026-04-25 10:30:56 +0700

    Create sorting_algorithms.py

---
+++ sorting_algorithms.py
@@ -0,0 +1,7 @@
+class SortingAlgorithms:
+  def __init__(self):
+    pass
+  #Mỗi cái sort return running time và number of comparisons ở dạng tuple cho đồng bộ nha mn (time, comp).
+  def selection_sort(L: list):
+    pass
+  
