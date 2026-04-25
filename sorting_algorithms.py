5ed71b191d69d071c8b9babe71c111db85af225f
daoviethung0911 <140297578+daoviethung0911@users.noreply.github.com>
2026-04-25 14:11:05 +0000

    update

--- sorting_algorithms.py
+++ sorting_algorithms.py
@@ -1,63 +1,63 @@
 #Mỗi cái sort return running time và number of comparisons ở dạng tuple cho đồng bộ nha mn (time, comp).
 def selection_sort(L: list):
-  pass
+    pass
 
 
 def insertion_sort(L: list):
-  pass
+    pass
 
 
 def binary_insertion_sort(L: list):
-  pass
+    pass
 
 
 def bubble_sort(L: list):
-  pass
+    pass
 
 
 def shaker_sort(L: list):
-  pass
+    pass
 
 
 def shell_sort(L: list):
-  pass
+    pass
 
 
 def heap_sort(L: list):
-  pass
+    pass
 
 
 def merge_sort(L: list):
-  pass
+    pass
 
 
 def quick_sort(L: list):
-  pass
+    pass
 
 
 def counting_sort(L: list):
-  pass
+    pass
 
 
 def radix_sort(L: list):
-  pass
+    pass
 
 
 def flash_sort(L: list):
-  pass
+    pass
 
 
 sorting_map = {
-  'selection-sort': selection_sort,
-  'insertion-sort': insertion_sort,
-  'binary-insertion-sort': binary_insertion_sort,
-  'bubble-sort': bubble_sort,
-  'shaker-sort': shaker_sort,
-  'shell-sort': shell_sort,
-  'heap-sort': heap_sort,
-  'merge-sort': merge_sort,
-  'quick-sort': quick_sort,
-  'counting-sort': counting_sort,
-  'radix-sort': radix_sort,
-  'flash-sort': flash_sort,
+    'selection-sort': selection_sort,
+    'insertion-sort': insertion_sort,
+    'binary-insertion-sort': binary_insertion_sort,
+    'bubble-sort': bubble_sort,
+    'shaker-sort': shaker_sort,
+    'shell-sort': shell_sort,
+    'heap-sort': heap_sort,
+    'merge-sort': merge_sort,
+    'quick-sort': quick_sort,
+    'counting-sort': counting_sort,
+    'radix-sort': radix_sort,
+    'flash-sort': flash_sort,
 }
