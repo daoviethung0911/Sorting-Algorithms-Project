148413a2b2233f7a509a57172bb2cd3d856888f8
daoviethung0911 <140297578+daoviethung0911@users.noreply.github.com>
2026-04-25 05:18:25 +0000

    update

--- sorting_algorithms.py
+++ sorting_algorithms.py
@@ -4,5 +4,33 @@ class SortingAlgorithms:
   #Mỗi cái sort return running time và number of comparisons ở dạng tuple cho đồng bộ nha mn (time, comp).
   def selection_sort(L: list):
     pass
-  pass
   
+  def binary_insertion_sort(L: list):
+    pass
+
+  def bubble_sort(L: list):
+    pass
+
+  def shaker_sort(L: list):
+    pass
+
+  def shell_sort(L: list):
+    pass
+
+  def heap_sort(L: list):
+    pass
+
+  def merge_sort(L: list):
+    pass
+
+  def quick_sort(L: list):
+    pass
+
+  def counting_sort(L: list):
+    pass
+
+  def radix_sort(L: list):
+    pass
+
+  def flash_sort(L: list):
+    pass
