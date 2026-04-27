6b25ef448af68412d17f8dfe8749e684b100c047
Nhatkoki <nhatkoki123@gmail.com>
2026-04-27 19:39:08 +0700

    Add command_3 for algorithm execution and output
    Implement command_3 to display algorithm details and performance metrics.

--- 07.py
+++ 07.py
@@ -34,7 +34,33 @@ def command_2(algo_name: str, input_size: int, input_order: str, output_param: s
 
 
 def command_3(algo_name: str, input_size: int, output_param: str):
-    pass
+    print("ALGORITHM MODE")
+    print(f"Algorithm: {algo_name}")
+    print(f"Input size: {input_size}")
+    print("-------------------------")
+
+    if algo_name not in sorting_map:
+        print(f"Lỗi: Không tìm thấy thuật toán '{algo_name}' trong hệ thống!")
+        return
+    sort_function = sorting_map[algo_name]
+
+    for order in ["sorted", "reverse", "nearly-sorted", "random"]:
+        print(f"Input order: {order}")
+        print('-------------')
+        arr = generate_data(input_size, data_type=order)
+        sorted_arr, time_comps = sort_function(arr)
+        exec_time, comps = time_comps
+
+        if output_param == "-time":
+            print(f"Running time (if required): {exec_time:.4f}")
+            print()
+        if output_param == "-comp":
+            print(f"Comparisons (if required): {comps}")
+            print()
+        if output_param == "-both":
+            print(f"Running time (if required): {exec_time:.4f}")
+            print(f"Comparisons (if required): {comps}")
+            print()
 
 
 def command_4(algo_1: str, algo_2: str, input_file: str):
