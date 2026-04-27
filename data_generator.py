cf20bd214ee4f8ff0b3ce4f67414c518cb8e8fc0
khiem2007 <84562074+khiem2007@users.noreply.github.com>
2026-04-27 20:08:50 +0700

    Add files via upload

--- data_generator.py
+++ data_generator.py
@@ -36,20 +36,6 @@ def generate_data(n, data_type):
         print("Error: unknown data type!")
         return None
 
-def generate_data_v2(n, data_type):
-    """Main function to generate data based on type."""
-    #["Randomize", "Nearly Sorted", "Sorted", "Reversed"]
-    if data_type == "Randomize":
-        return generate_random_data(n)
-    elif data_type == "Sorted":
-        return generate_sorted_data(n)
-    elif data_type == "Reversed":
-        return generate_reverse_data(n)
-    elif data_type == "Nearly Sorted":
-        return generate_nearly_sorted_data(n)
-    else:
-        print("Error: unknown data type!")
-        return None
 # Usage example:
 # n = 10
 # random_array = generate_data(n, 0)
