7fac70666a55aea894170cd56c6ddcf37b0dbd46
daoviethung0911 <140297578+daoviethung0911@users.noreply.github.com>
2026-04-28 16:39:13 +0000

    update

--- file_io.py
+++ file_io.py
@@ -5,6 +5,6 @@ def read_input_file(filename: str):
     return data
 
 def write_output_file(filename: str, data: list[int]):
-    with open(filename, 'w', encoding = 'utf-8') as f:
+    with open(filename, 'a', encoding = 'utf-8') as f:
         f.write(f'{len(data)}\n')
-        f.write(' '.join(map(str, data)))
+        f.write(' '.join(map(str, data)) + '\n')
